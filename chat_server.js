5fefc4dc046c6aef1a8bea5d7e0b2eaf947a69a1
Joshua Shapiro <jshapiro1@Student1010-6.local>
2020-05-05 16:44:04 -0400

    done

---
+++ chat_server.js
@@ -0,0 +1,34 @@
+var players = [];
+var leader = "";
+// Require the packages we will use:
+var http = require("http"),
+	socketio = require("socket.io"),
+	fs = require("fs");
+
+// Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
+var app = http.createServer(function(req, resp){
+	// This callback runs when a new connection is made to our HTTP server.
+	
+	fs.readFile("client.html", function(err, data){
+		// This callback runs when the client.html file has been read from the filesystem.
+		
+		if(err) return resp.writeHead(500);
+		resp.writeHead(200);
+		resp.end(data);
+	});
+});
+app.listen(3456);
+
+// Do the Socket.IO magic:
+var io = socketio.listen(app);
+io.sockets.on("connection", function(socket){
+	// This callback runs when a new Socket.IO connection is established.
+	
+	socket.on('add_player_to_server', function(data) {
+		if (players.length==0) {
+			leader = data['username'];
+        }
+		players.push(data['username']);
+		io.sockets.emit("add_player_to_client", { players: players, leader: leader}) // broadcast the message to other users
+	});
+});
